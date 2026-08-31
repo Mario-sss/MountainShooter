3c646036edef83a7d30deb97191f1b24885c59a7
Mario-sss <mario.silva291219@gmail.com>
2026-08-31 08:29:54 -0300

    Hit box collisions Hud Score

--- code/level.py
+++ code/level.py
@@ -3,7 +3,7 @@ import sys
 import pygame
 from pygame import Surface, Rect
 from pygame.ftfont import Font
-from code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
+from code.Const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME, C_GREEN, C_CYAN
 from code.EntityMediator import EntityMediator
 from code.enemy import Enemy
 from code.entity import Entity
@@ -37,6 +37,10 @@ class Level:
                     shoot = ent.shoot()
                     if shoot is not None:
                         self.entity_list.append(shoot)
+                if ent.name == "Player1":
+                    self.level_text(14, f"Player1 - Health: {ent.health} | score: {ent.score}", C_GREEN, (10, 25))
+                if ent.name == "Player2":
+                    self.level_text(14, f"Player2 - Health: {ent.health} | score: {ent.score}", C_CYAN, (10, 45))
             # Events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -47,9 +51,9 @@ class Level:
                     self.entity_list.append(EntityFactory.get_entity(choice))
 
             # Printed text
-            self.level_text(14, f"{self.name} - timeout: {self.timeout / 1000 :1f}s", COLOR_WHITE, (10, 5))
-            self.level_text(14, f"fps: {clock.get_fps()}", COLOR_WHITE, (10, WIN_HEIGHT - 35))
-            self.level_text(14, f"entidades: {len(self.entity_list)}", COLOR_WHITE, (10, WIN_HEIGHT - 20))
+            self.level_text(14, f"{self.name} - timeout: {self.timeout / 1000 :1f}s", C_WHITE, (10, 5))
+            self.level_text(14, f"fps: {clock.get_fps()}", C_WHITE, (10, WIN_HEIGHT - 35))
+            self.level_text(14, f"entidades: {len(self.entity_list)}", C_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
             # Collitions
             EntityMediator.verify_collision(self.entity_list)

dd7b01235e748d6b1c4fa179739ed91a79d721ca
Mario-sss <mario.silva291219@gmail.com>
2026-09-02 00:03:24 -0300

    SQLlite3 implementation for Score using proxy design pattern Resolution of a bug in menu selection

--- code/Const.py
+++ code/Const.py
@@ -1,5 +1,6 @@
 # C
 import pygame
+from pygame.examples.grid import WINDOW_WIDTH
 
 C_ORANGE = (255, 128, 0)
 C_WHITE = (255, 255, 255)
@@ -49,9 +50,9 @@ ENTITY_HEALTH = {
     "Level2Bg3": 999,
     "Level2Bg4": 999,
     "Level2Bg5": 999,
-    "Player1": 300,
+    "Player1": 200,
     "Player1Shot": 1,
-    "Player2": 300,
+    "Player2": 100,
     "Player2Shot": 1,
     "Enemy1": 50,
     "Enemy1Shot": 1,
@@ -141,3 +142,21 @@ TIMEOUT_LEVEL = 20000 #20s
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+#S
+SCORE_POS = {
+    "Title": (WIN_WIDTH/2, 50),
+    "EnterName":(WIN_WIDTH/2, 80),
+    "Label":(WIN_WIDTH/2, 90),
+    "Name": (WIN_WIDTH/2, 110),
+    0: (WIN_WIDTH/2, 110),
+    1: (WIN_WIDTH/2, 130),
+    2: (WIN_WIDTH/2, 150),
+    3: (WIN_WIDTH/2, 170),
+    4: (WIN_WIDTH/2, 190),
+    5:(WIN_WIDTH/2, 210),
+    6: (WIN_WIDTH/2, 230),
+    7: (WIN_WIDTH/2, 250),
+    8: (WIN_WIDTH/2, 270),
+    9: (WIN_WIDTH/2, 290),
+}
